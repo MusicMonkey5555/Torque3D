56fce8aa14d683cf6d17953b007be91adf3f2dbc
Nathan Bowhay <skienemy5555@gmail.com>
2015-02-03 15:24:49 -0800

    Added MatrixD class
    Added matrix double class

--- Engine/source/math/mMatrix.cpp
+++ Engine/source/math/mMatrix.cpp
@@ -193,3 +193,170 @@ void MatrixF::dumpMatrix(const char *caption /* =NULL */) const
    Con::printf("%s   | %-8.4f %-8.4f %-8.4f %-8.4f |", spacerRef,  m[idx(2,0)], m[idx(2, 1)], m[idx(2, 2)], m[idx(2, 3)]);
    Con::printf("%s   | %-8.4f %-8.4f %-8.4f %-8.4f |", spacerRef,  m[idx(3,0)], m[idx(3, 1)], m[idx(3, 2)], m[idx(3, 3)]);
 }
+
+const MatrixD MatrixD::Identity( true );
+
+// idx(i,j) is index to element in column i, row j
+
+void MatrixD::transposeTo(F64 *matrix) const
+{
+   matrix[idx(0,0)] = m[idx(0,0)];
+   matrix[idx(0,1)] = m[idx(1,0)];
+   matrix[idx(0,2)] = m[idx(2,0)];
+   matrix[idx(0,3)] = m[idx(3,0)];
+   matrix[idx(1,0)] = m[idx(0,1)];
+   matrix[idx(1,1)] = m[idx(1,1)];
+   matrix[idx(1,2)] = m[idx(2,1)];
+   matrix[idx(1,3)] = m[idx(3,1)];
+   matrix[idx(2,0)] = m[idx(0,2)];
+   matrix[idx(2,1)] = m[idx(1,2)];
+   matrix[idx(2,2)] = m[idx(2,2)];
+   matrix[idx(2,3)] = m[idx(3,2)];
+   matrix[idx(3,0)] = m[idx(0,3)];
+   matrix[idx(3,1)] = m[idx(1,3)];
+   matrix[idx(3,2)] = m[idx(2,3)];
+   matrix[idx(3,3)] = m[idx(3,3)];
+}
+
+bool MatrixD::isAffine() const
+{
+   // An affine transform is defined by the following structure
+   //
+   // [ X X X P ]
+   // [ X X X P ]
+   // [ X X X P ]
+   // [ 0 0 0 1 ]
+   //
+   //  Where X is an orthonormal 3x3 submatrix and P is an arbitrary translation
+   //  We'll check in the following order:
+   //   1: [3][3] must be 1
+   //   2: Shear portion must be zero
+   //   3: Dot products of rows and columns must be zero
+   //   4: Length of rows and columns must be 1
+   //
+   if (m[idx(3,3)] != 1.0)
+      return false;
+
+   if (m[idx(0,3)] != 0.0 ||
+       m[idx(1,3)] != 0.0 ||
+       m[idx(2,3)] != 0.0)
+      return false;
+
+   Point3D one, two, three;
+   getColumn(0, &one);
+   getColumn(1, &two);
+   getColumn(2, &three);
+   if (mDot(one, two)   > 0.0001 ||
+       mDot(one, three) > 0.0001 ||
+       mDot(two, three) > 0.0001)
+      return false;
+
+   if (mFabs(1.0 - one.lenSquared()) > 0.0001 ||
+       mFabs(1.0 - two.lenSquared()) > 0.0001 ||
+       mFabs(1.0 - three.lenSquared()) > 0.0001)
+      return false;
+
+   getRow(0, &one);
+   getRow(1, &two);
+   getRow(2, &three);
+   if (mDot(one, two)   > 0.0001 ||
+       mDot(one, three) > 0.0001 ||
+       mDot(two, three) > 0.0001)
+      return false;
+
+   if (mFabs(1.0 - one.lenSquared()) > 0.0001f ||
+       mFabs(1.0 - two.lenSquared()) > 0.0001f ||
+       mFabs(1.0 - three.lenSquared()) > 0.0001f)
+      return false;
+
+   // We're ok.
+   return true;
+}
+
+// Perform inverse on full 4x4 matrix.  Used in special cases only, so not at all optimized.
+bool MatrixD::fullInverse()
+{
+   Point4D a,b,c,d;
+   getRow(0,&a);
+   getRow(1,&b);
+   getRow(2,&c);
+   getRow(3,&d);
+
+   // det = a0*b1*c2*d3 - a0*b1*c3*d2 - a0*c1*b2*d3 + a0*c1*b3*d2 + a0*d1*b2*c3 - a0*d1*b3*c2 -
+   //       b0*a1*c2*d3 + b0*a1*c3*d2 + b0*c1*a2*d3 - b0*c1*a3*d2 - b0*d1*a2*c3 + b0*d1*a3*c2 +
+   //       c0*a1*b2*d3 - c0*a1*b3*d2 - c0*b1*a2*d3 + c0*b1*a3*d2 + c0*d1*a2*b3 - c0*d1*a3*b2 -
+   //       d0*a1*b2*c3 + d0*a1*b3*c2 + d0*b1*a2*c3 - d0*b1*a3*c2 - d0*c1*a2*b3 + d0*c1*a3*b2
+   F64 det = a.x*b.y*c.z*d.w - a.x*b.y*c.w*d.z - a.x*c.y*b.z*d.w + a.x*c.y*b.w*d.z + a.x*d.y*b.z*c.w - a.x*d.y*b.w*c.z
+           - b.x*a.y*c.z*d.w + b.x*a.y*c.w*d.z + b.x*c.y*a.z*d.w - b.x*c.y*a.w*d.z - b.x*d.y*a.z*c.w + b.x*d.y*a.w*c.z
+           + c.x*a.y*b.z*d.w - c.x*a.y*b.w*d.z - c.x*b.y*a.z*d.w + c.x*b.y*a.w*d.z + c.x*d.y*a.z*b.w - c.x*d.y*a.w*b.z
+           - d.x*a.y*b.z*c.w + d.x*a.y*b.w*c.z + d.x*b.y*a.z*c.w - d.x*b.y*a.w*c.z - d.x*c.y*a.z*b.w + d.x*c.y*a.w*b.z;
+
+   if (mFabs(det)<0.00001)
+      return false;
+
+   Point4D aa,bb,cc,dd;
+   aa.x =  b.y*c.z*d.w - b.y*c.w*d.z - c.y*b.z*d.w + c.y*b.w*d.z + d.y*b.z*c.w - d.y*b.w*c.z;
+   aa.y = -a.y*c.z*d.w + a.y*c.w*d.z + c.y*a.z*d.w - c.y*a.w*d.z - d.y*a.z*c.w + d.y*a.w*c.z;
+   aa.z =  a.y*b.z*d.w - a.y*b.w*d.z - b.y*a.z*d.w + b.y*a.w*d.z + d.y*a.z*b.w - d.y*a.w*b.z;
+   aa.w = -a.y*b.z*c.w + a.y*b.w*c.z + b.y*a.z*c.w - b.y*a.w*c.z - c.y*a.z*b.w + c.y*a.w*b.z;
+
+   bb.x = -b.x*c.z*d.w + b.x*c.w*d.z + c.x*b.z*d.w - c.x*b.w*d.z - d.x*b.z*c.w + d.x*b.w*c.z;
+   bb.y =  a.x*c.z*d.w - a.x*c.w*d.z - c.x*a.z*d.w + c.x*a.w*d.z + d.x*a.z*c.w - d.x*a.w*c.z;
+   bb.z = -a.x*b.z*d.w + a.x*b.w*d.z + b.x*a.z*d.w - b.x*a.w*d.z - d.x*a.z*b.w + d.x*a.w*b.z;
+   bb.w =  a.x*b.z*c.w - a.x*b.w*c.z - b.x*a.z*c.w + b.x*a.w*c.z + c.x*a.z*b.w - c.x*a.w*b.z;
+
+   cc.x =  b.x*c.y*d.w - b.x*c.w*d.y - c.x*b.y*d.w + c.x*b.w*d.y + d.x*b.y*c.w - d.x*b.w*c.y;
+   cc.y = -a.x*c.y*d.w + a.x*c.w*d.y + c.x*a.y*d.w - c.x*a.w*d.y - d.x*a.y*c.w + d.x*a.w*c.y;
+   cc.z =  a.x*b.y*d.w - a.x*b.w*d.y - b.x*a.y*d.w + b.x*a.w*d.y + d.x*a.y*b.w - d.x*a.w*b.y;
+   cc.w = -a.x*b.y*c.w + a.x*b.w*c.y + b.x*a.y*c.w - b.x*a.w*c.y - c.x*a.y*b.w + c.x*a.w*b.y;
+
+   dd.x = -b.x*c.y*d.z + b.x*c.z*d.y + c.x*b.y*d.z - c.x*b.z*d.y - d.x*b.y*c.z + d.x*b.z*c.y;
+   dd.y =  a.x*c.y*d.z - a.x*c.z*d.y - c.x*a.y*d.z + c.x*a.z*d.y + d.x*a.y*c.z - d.x*a.z*c.y;
+   dd.z = -a.x*b.y*d.z + a.x*b.z*d.y + b.x*a.y*d.z - b.x*a.z*d.y - d.x*a.y*b.z + d.x*a.z*b.y;
+   dd.w =  a.x*b.y*c.z - a.x*b.z*c.y - b.x*a.y*c.z + b.x*a.z*c.y + c.x*a.y*b.z - c.x*a.z*b.y;
+
+   setRow(0,aa);
+   setRow(1,bb);
+   setRow(2,cc);
+   setRow(3,dd);
+
+   mul(1.0/det);
+
+   return true;
+}
+
+EulerD MatrixD::toEuler() const
+{
+   const F64 * mat = m;
+
+   EulerD r;
+   r.x = mAsin(mClampF(mat[MatrixD::idx(2,1)], -1.0, 1.0));
+
+   if(mCos(r.x) != 0.0)
+   {
+      r.y = mAtan2(-mat[MatrixD::idx(2,0)], mat[MatrixD::idx(2,2)]);
+      r.z = mAtan2(-mat[MatrixD::idx(0,1)], mat[MatrixD::idx(1,1)]);
+   }
+   else
+   {
+      r.y = 0.f;
+      r.z = mAtan2(mat[MatrixD::idx(1,0)], mat[MatrixD::idx(0,0)]);
+   }
+
+   return r;
+}
+
+void MatrixD::dumpMatrix(const char *caption /* =NULL */) const
+{
+   U32 size = dStrlen(caption);
+   FrameTemp<char> spacer(size+1);
+   char *spacerRef = spacer;
+
+   dMemset(spacerRef, ' ', size);
+   spacerRef[size] = 0;
+
+   Con::printf("%s = | %-8.4f %-8.4f %-8.4f %-8.4f |", caption,    m[idx(0,0)], m[idx(0, 1)], m[idx(0, 2)], m[idx(0, 3)]);
+   Con::printf("%s   | %-8.4f %-8.4f %-8.4f %-8.4f |", spacerRef,  m[idx(1,0)], m[idx(1, 1)], m[idx(1, 2)], m[idx(1, 3)]);
+   Con::printf("%s   | %-8.4f %-8.4f %-8.4f %-8.4f |", spacerRef,  m[idx(2,0)], m[idx(2, 1)], m[idx(2, 2)], m[idx(2, 3)]);
+   Con::printf("%s   | %-8.4f %-8.4f %-8.4f %-8.4f |", spacerRef,  m[idx(3,0)], m[idx(3, 1)], m[idx(3, 2)], m[idx(3, 3)]);
+}
--- Engine/source/math/mMatrix.h
+++ Engine/source/math/mMatrix.h
@@ -588,4 +588,636 @@ inline void mTransformPlane(const MatrixF& mat, const Point3F& scale, const Plan
    m_matF_x_scale_x_planeF(mat, &scale.x, &plane.x, &result->x);
 }
 
+/// 4x4 Matrix Class
+///
+/// This runs at F64 precision.
+
+class MatrixD
+{
+private:
+   F64 m[16];     ///< Note: Torque uses row-major matrices
+
+public:
+   /// Create an uninitialized matrix.
+   ///
+   /// @param   identity    If true, initialize to the identity matrix.
+   explicit MatrixD(bool identity=false);
+
+   /// Create a matrix to rotate about origin by e.
+   /// @see set
+   explicit MatrixD( const EulerD &e);
+
+   /// Create a matrix to rotate about p by e.
+   /// @see set
+   MatrixD( const EulerD &e, const Point3D& p);
+
+   MatrixD( const MatrixF &mat);
+
+   MatrixD(F64* openGLMatrix);
+
+   /// Get the index in m to element in column i, row j
+   ///
+   /// This is necessary as we have m as a one dimensional array.
+   ///
+   /// @param   i   Column desired.
+   /// @param   j   Row desired.
+   static U32 idx(U32 i, U32 j) { return (i + j*4); }
+
+   /// Initialize matrix to rotate about origin by e.
+   MatrixD& set( const EulerD &e);
+
+   /// Initialize matrix to rotate about p by e.
+   MatrixD& set( const EulerD &e, const Point3D& p);
+
+   MatrixD& set( const MatrixF &mat);
+
+   bool equals(const MatrixD& mat);
+
+   /// Initialize matrix with a cross product of p.
+   MatrixD& setCrossProduct( const Point3D &p);
+
+   /// Initialize matrix with a tensor product of p.
+   MatrixD& setTensorProduct( const Point3D &p, const Point3D& q);
+
+   operator F64*() { return (m); }              ///< Allow people to get at m.
+   operator const F64*() const { return (F64*)(m); }  ///< Allow people to get at m.
+
+   bool isAffine() const;                       ///< Check to see if this is an affine matrix.
+   bool isIdentity() const;                     ///< Checks for identity matrix.
+
+   /// Make this an identity matrix.
+   MatrixD& identity();
+
+   /// Invert m.
+   MatrixD& inverse();
+   /// Copy the inversion of this into out matrix.
+   void invertTo( MatrixD *out );
+
+   /// Take inverse of matrix assuming it is affine (rotation,
+   /// scale, sheer, translation only).
+   MatrixD& affineInverse();           
+
+   /// Swap rows and columns.
+   MatrixD& transpose();
+
+   /// M * Matrix(p) -> M
+   MatrixD& scale( const Point3D &s );            
+   MatrixD& scale( F64 s ) { return scale( Point3D( s, s, s ) ); }
+
+   /// Return scale assuming scale was applied via mat.scale(s).
+   Point3D getScale() const;
+
+   EulerD toEuler() const;
+
+   /// Compute the inverse of the matrix.
+   ///
+   /// Computes inverse of full 4x4 matrix. Returns false and performs no inverse if
+   /// the determinant is 0.
+   ///
+   /// Note: In most cases you want to use the normal inverse function.  This method should
+   ///       be used if the matrix has something other than (0,0,0,1) in the bottom row.
+   bool fullInverse();
+
+   /// Swaps rows and columns into matrix.
+   void transposeTo(F64 *matrix) const;
+
+   /// Normalize the matrix.
+   void normalize();
+
+   /// Copy the requested column into a Point4D.
+   void getColumn(S32 col, Point4D *cptr) const;
+   Point4D getColumn4D(S32 col) const { Point4D ret; getColumn(col,&ret); return ret; }
+
+   /// Copy the requested column into a Point3D.
+   ///
+   /// This drops the bottom-most row.
+   void getColumn(S32 col, Point3D *cptr) const;
+   Point3D getColumn3D(S32 col) const { Point3D ret; getColumn(col,&ret); return ret; }
+
+   /// Set the specified column from a Point4D.
+   void setColumn(S32 col, const Point4D& cptr);
+
+   /// Set the specified column from a Point3D.
+   ///
+   /// The bottom-most row is not set.
+   void setColumn(S32 col, const Point3D& cptr);
+
+   /// Copy the specified row into a Point4D.
+   void getRow(S32 row, Point4D *cptr) const;
+   Point4D getRow4D(S32 row) const { Point4D ret; getRow(row,&ret); return ret; }
+
+   /// Copy the specified row into a Point3D.
+   ///
+   /// Right-most item is dropped.
+   void getRow(S32 row, Point3D *cptr) const;
+   Point3D getRow3D(S32 row) const { Point3D ret; getRow(row,&ret); return ret; }
+
+   /// Set the specified row from a Point4D.
+   void setRow(S32 row, const Point4D& cptr);
+
+   /// Set the specified row from a Point3D.
+   ///
+   /// The right-most item is not set.
+   void setRow(S32 row, const Point3D& cptr);
+
+   /// Get the position of the matrix.
+   ///
+   /// This is the 4th column of the matrix.
+   Point3D getPosition() const;
+
+   /// Set the position of the matrix.
+   ///
+   /// This is the 4th column of the matrix.
+   void setPosition( const Point3D &pos ) { setColumn( 3, pos ); }
+
+   /// Add the passed delta to the matrix position.
+   void displace( const Point3D &delta );
+
+   /// Get the x axis of the matrix.
+   ///
+   /// This is the 1st column of the matrix and is
+   /// normally considered the right vector.
+   VectorD getRightVector() const;
+
+   /// Get the y axis of the matrix.
+   ///
+   /// This is the 2nd column of the matrix and is
+   /// normally considered the forward vector.   
+   VectorD getForwardVector() const;   
+
+   /// Get the z axis of the matrix.
+   ///
+   /// This is the 3rd column of the matrix and is
+   /// normally considered the up vector.   
+   VectorD getUpVector() const;
+
+   MatrixD&  mul(const MatrixD &a);                    ///< M * a -> M
+   MatrixD&  mulL(const MatrixD &a);                   ///< a * M -> M
+   MatrixD&  mul(const MatrixD &a, const MatrixD &b);  ///< a * b -> M
+
+   // Scalar multiplies
+   MatrixD&  mul(const F64 a);                         ///< M * a -> M
+   MatrixD&  mul(const MatrixD &a, const F64 b);       ///< a * b -> M
+
+
+   void mul( Point4D& p ) const;                       ///< M * p -> p (full [4x4] * [1x4])
+   void mulP( Point3D& p ) const;                      ///< M * p -> p (assume w = 1.0f)
+   void mulP( const Point3D &p, Point3D *d) const;     ///< M * p -> d (assume w = 1.0f)
+   void mulV( VectorD& p ) const;                      ///< M * v -> v (assume w = 0.0f)
+   void mulV( const VectorD &p, Point3D *d) const;     ///< M * v -> d (assume w = 0.0f)
+
+   void mul(Box3D& b) const;                           ///< Axial box -> Axial Box
+   
+   MatrixD& add( const MatrixD& m );
+
+   /// Convenience function to allow people to treat this like an array.
+   F64& operator ()(S32 row, S32 col) { return m[idx(col,row)]; }
+   F64 operator ()(S32 row, S32 col) const { return m[idx(col,row)]; }
+   
+   bool operator==(const MatrixD& a) const
+   {
+      return (a[0] == m[0] && a[1] == m[1] && a[2] == m[2] && a[3] == m[3]
+         && a[4] == m[4] && a[5] == m[5] && a[6] == m[6] && a[7] == m[7]
+         && a[8] == m[8] && a[9] == m[9] && a[10] == m[10] && a[11] == m[11]
+         && a[12] == m[12] && a[13] == m[13] && a[14] == m[14] && a[15] == m[15]);
+   }
+
+   bool operator!=(const MatrixD& a) const
+   {
+      return (a[0] != m[0] || a[1] != m[1] || a[2] != m[2] || a[3] != m[3]
+         || a[4] != m[4] || a[5] != m[5] || a[6] != m[6] || a[7] != m[7]
+         || a[8] != m[8] || a[9] != m[9] || a[10] != m[10] || a[11] != m[11]
+         || a[12] != m[12] || a[13] != m[13] || a[14] != m[14] || a[15] != m[15]);
+   }
+   
+   void dumpMatrix(const char *caption=NULL) const;
+
+   // Math operator overloads
+   //------------------------------------
+   friend MatrixD operator * ( const MatrixD &m1, const MatrixD &m2 );
+   MatrixD& operator *= ( const MatrixD &m );
+
+   // Static identity matrix
+   const static MatrixD Identity;
+};
+
+
+//--------------------------------------
+// Inline Functions
+
+inline MatrixD::MatrixD(bool _identity)
+{
+   if (_identity)
+      identity();
+}
+
+inline MatrixD::MatrixD( const EulerD &e )
+{
+   set(e);
+}
+
+inline MatrixD::MatrixD( const MatrixF &mat)
+{
+	set(mat);
+}
+
+inline MatrixD::MatrixD( const EulerD &e, const Point3D& p )
+{
+   set(e,p);
+}
+
+inline MatrixD::MatrixD(F64* openGLMatrix)
+{
+   m[0] = openGLMatrix[0];
+   m[1] = openGLMatrix[4];
+   m[2] = openGLMatrix[8];
+   m[3] = openGLMatrix[12];
+   m[4] = openGLMatrix[1];
+   m[5] = openGLMatrix[5];
+   m[6] = openGLMatrix[9];
+   m[7] = openGLMatrix[13];
+   m[8] = openGLMatrix[2];
+   m[9] = openGLMatrix[6];
+   m[10] = openGLMatrix[10];
+   m[11] = openGLMatrix[14];
+   m[12] = openGLMatrix[3];
+   m[13] = openGLMatrix[7];
+   m[14] = openGLMatrix[11];
+   m[15] = openGLMatrix[15];
+}
+
+inline MatrixD& MatrixD::set( const EulerD &e)
+{
+   m_matD_set_euler( e, *this );
+   return (*this);
+}
+
+
+inline MatrixD& MatrixD::set( const EulerD &e, const Point3D& p)
+{
+   m_matD_set_euler_point( e, p, *this );
+   return (*this);
+}
+
+inline MatrixD& MatrixD::set( const MatrixF &mat)
+{
+   m[0] = mat[0];
+   m[1] = mat[1];
+   m[2] = mat[2];
+   m[3] = mat[3];
+   m[4] = mat[4];
+   m[5] = mat[5];
+   m[6] = mat[6];
+   m[7] = mat[7];
+   m[8] = mat[8];
+   m[9] = mat[9];
+   m[10] = mat[10];
+   m[11] = mat[11];
+   m[12] = mat[12];
+   m[13] = mat[13];
+   m[14] = mat[14];
+   m[15] = mat[15];
+
+   return (*this);
+}
+
+inline bool MatrixD::equals(const MatrixD& mat)
+{
+   return (m[0] == mat[0] && m[1] == mat[1] && m[2] == mat[2] && m[3] == mat[3] &&
+      m[4] == mat[4] && m[5] == mat[5] && m[6] == mat[6] && m[7] == mat[7] &&
+      m[8] == mat[8] && m[9] == mat[9] && m[10] == mat[10] && m[11] == mat[11] &&
+      m[12] == mat[12] && m[13] == mat[13] && m[14] == mat[14] && m[15] == mat[15]);
+}
+
+inline MatrixD& MatrixD::setCrossProduct( const Point3D &p)
+{
+   m[1] = -(m[4] = p.z);
+   m[8] = -(m[2] = p.y);
+   m[6] = -(m[9] = p.x);
+   m[0] = m[3] = m[5] = m[7] = m[10] = m[11] =
+      m[12] = m[13] = m[14] = 0.0;
+   m[15] = 1;
+   return (*this);
+}
+
+inline MatrixD& MatrixD::setTensorProduct( const Point3D &p, const Point3D &q)
+{
+   m[0] = p.x * q.x;
+   m[1] = p.x * q.y;
+   m[2] = p.x * q.z;
+   m[4] = p.y * q.x;
+   m[5] = p.y * q.y;
+   m[6] = p.y * q.z;
+   m[8] = p.z * q.x;
+   m[9] = p.z * q.y;
+   m[10] = p.z * q.z;
+   m[3] = m[7] = m[11] = m[12] = m[13] = m[14] = 0.0;
+   m[15] = 1.0;
+   return (*this);
+}
+
+inline bool MatrixD::isIdentity() const
+{
+   return
+   m[0]  == 1.0 &&
+   m[1]  == 0.0 &&
+   m[2]  == 0.0 &&
+   m[3]  == 0.0 &&
+   m[4]  == 0.0 &&
+   m[5]  == 1.0 &&
+   m[6]  == 0.0 &&
+   m[7]  == 0.0 &&
+   m[8]  == 0.0 &&
+   m[9]  == 0.0 &&
+   m[10] == 1.0 &&
+   m[11] == 0.0 &&
+   m[12] == 0.0 &&
+   m[13] == 0.0 &&
+   m[14] == 0.0 &&
+   m[15] == 1.0;
+}
+
+inline MatrixD& MatrixD::identity()
+{
+   m[0]  = 1.0;
+   m[1]  = 0.0;
+   m[2]  = 0.0;
+   m[3]  = 0.0;
+   m[4]  = 0.0;
+   m[5]  = 1.0;
+   m[6]  = 0.0;
+   m[7]  = 0.0;
+   m[8]  = 0.0;
+   m[9]  = 0.0;
+   m[10] = 1.0;
+   m[11] = 0.0;
+   m[12] = 0.0;
+   m[13] = 0.0;
+   m[14] = 0.0;
+   m[15] = 1.0;
+   return (*this);
+}
+
+
+inline MatrixD& MatrixD::inverse()
+{
+   m_matD_inverse(m);
+   return (*this);
+}
+
+inline void MatrixD::invertTo( MatrixD *out )
+{
+   m_matD_invert_to(m,*out);
+}
+
+inline MatrixD& MatrixD::affineInverse()
+{
+//   AssertFatal(isAffine() == true, "Error, this matrix is not an affine transform");
+   m_matD_affineInverse(m);
+   return (*this);
+}
+
+inline MatrixD& MatrixD::transpose()
+{
+   m_matD_transpose(m);
+   return (*this);
+}
+
+inline MatrixD& MatrixD::scale(const Point3D& p)
+{
+   m_matD_scale(m,p);
+   return *this;
+}
+
+inline Point3D MatrixD::getScale() const
+{
+   Point3D scale;
+   scale.x = mSqrt(m[0]*m[0] + m[4] * m[4] + m[8] * m[8]);
+   scale.y = mSqrt(m[1]*m[1] + m[5] * m[5] + m[9] * m[9]);
+   scale.z = mSqrt(m[2]*m[2] + m[6] * m[6] + m[10] * m[10]);
+   return scale;
+}
+
+inline void MatrixD::normalize()
+{
+   m_matD_normalize(m);
+}
+
+inline MatrixD& MatrixD::mul( const MatrixD &a )
+{  // M * a -> M
+   AssertFatal(&a != this, "MatrixD::mul - a.mul(a) is invalid!");
+
+   MatrixD tempThis(*this);
+   m_matD_x_matD(tempThis, a, *this);
+   return (*this);
+}
+
+inline MatrixD& MatrixD::mulL( const MatrixD &a )
+{  // a * M -> M
+   AssertFatal(&a != this, "MatrixD::mulL - a.mul(a) is invalid!");
+
+   MatrixD tempThis(*this);
+   m_matD_x_matD(a, tempThis, *this);
+   return (*this);
+}
+
+inline MatrixD& MatrixD::mul( const MatrixD &a, const MatrixD &b )
+{  // a * b -> M
+   AssertFatal((&a != this) && (&b != this), "MatrixD::mul - a.mul(a, b) a.mul(b, a) a.mul(a, a) is invalid!");
+
+   m_matD_x_matD(a, b, *this);
+   return (*this);
+}
+
+
+inline MatrixD& MatrixD::mul(const F64 a)
+{
+   for (U32 i = 0; i < 16; i++)
+      m[i] *= a;
+
+   return *this;
+}
+
+
+inline MatrixD& MatrixD::mul(const MatrixD &a, const F64 b)
+{
+   *this = a;
+   mul(b);
+
+   return *this;
+}
+
+inline void MatrixD::mul( Point4D& p ) const
+{
+   Point4D temp;
+   m_matD_x_point4D(*this, &p.x, &temp.x);
+   p = temp;
+}
+
+inline void MatrixD::mulP( Point3D& p) const
+{
+   // M * p -> d
+   Point3D d;
+   m_matD_x_point3D(*this, &p.x, &d.x);
+   p = d;
+}
+
+inline void MatrixD::mulP( const Point3D &p, Point3D *d) const
+{
+   // M * p -> d
+   m_matD_x_point3D(*this, &p.x, &d->x);
+}
+
+inline void MatrixD::mulV( VectorD& v) const
+{
+   // M * v -> v
+   VectorD temp;
+   m_matD_x_vectorD(*this, &v.x, &temp.x);
+   v = temp;
+}
+
+inline void MatrixD::mulV( const VectorD &v, Point3D *d) const
+{
+   // M * v -> d
+   m_matD_x_vectorD(*this, &v.x, &d->x);
+}
+
+inline void MatrixD::mul(Box3D& b) const
+{
+   m_matD_x_box3D(*this, &b.minExtents.x, &b.maxExtents.x);
+}
+
+inline MatrixD& MatrixD::add( const MatrixD& a )
+{
+   for( U32 i = 0; i < 16; ++ i )
+      m[ i ] += a.m[ i ];
+      
+   return *this;
+}
+
+inline void MatrixD::getColumn(S32 col, Point4D *cptr) const
+{
+   cptr->x = m[col];
+   cptr->y = m[col+4];
+   cptr->z = m[col+8];
+   cptr->w = m[col+12];
+}
+
+inline void MatrixD::getColumn(S32 col, Point3D *cptr) const
+{
+   cptr->x = m[col];
+   cptr->y = m[col+4];
+   cptr->z = m[col+8];
+}
+
+inline void MatrixD::setColumn(S32 col, const Point4D &cptr)
+{
+   m[col]   = cptr.x;
+   m[col+4] = cptr.y;
+   m[col+8] = cptr.z;
+   m[col+12]= cptr.w;
+}
+
+inline void MatrixD::setColumn(S32 col, const Point3D &cptr)
+{
+   m[col]   = cptr.x;
+   m[col+4] = cptr.y;
+   m[col+8] = cptr.z;
+}
+
+
+inline void MatrixD::getRow(S32 col, Point4D *cptr) const
+{
+   col *= 4;
+   cptr->x = m[col++];
+   cptr->y = m[col++];
+   cptr->z = m[col++];
+   cptr->w = m[col];
+}
+
+inline void MatrixD::getRow(S32 col, Point3D *cptr) const
+{
+   col *= 4;
+   cptr->x = m[col++];
+   cptr->y = m[col++];
+   cptr->z = m[col];
+}
+
+inline void MatrixD::setRow(S32 col, const Point4D &cptr)
+{
+   col *= 4;
+   m[col++] = cptr.x;
+   m[col++] = cptr.y;
+   m[col++] = cptr.z;
+   m[col]   = cptr.w;
+}
+
+inline void MatrixD::setRow(S32 col, const Point3D &cptr)
+{
+   col *= 4;
+   m[col++] = cptr.x;
+   m[col++] = cptr.y;
+   m[col]   = cptr.z;
+}
+
+inline Point3D MatrixD::getPosition() const
+{
+   return Point3D( m[3], m[3+4], m[3+8] );
+}
+
+inline void MatrixD::displace( const Point3D &delta )
+{
+   m[3]   += delta.x;
+   m[3+4] += delta.y;
+   m[3+8] += delta.z;
+}
+
+inline VectorD MatrixD::getForwardVector() const
+{
+   VectorD vec;
+   getColumn( 1, &vec );
+   return vec;
+}
+
+inline VectorD MatrixD::getRightVector() const
+{
+   VectorD vec;
+   getColumn( 0, &vec );
+   return vec;
+}
+
+inline VectorD MatrixD::getUpVector() const
+{
+   VectorD vec;
+   getColumn( 2, &vec );
+   return vec;
+}
+
+//------------------------------------
+// Math operator overloads
+//------------------------------------
+inline MatrixD operator * ( const MatrixD &m1, const MatrixD &m2 )
+{
+   // temp = m1 * m2
+   MatrixD temp;
+   m_matD_x_matD(m1, m2, temp);
+   return temp;
+}
+
+inline MatrixD& MatrixD::operator *= ( const MatrixD &m )
+{
+   MatrixD tempThis(*this);
+   m_matD_x_matD(tempThis, m, *this);
+   return (*this);
+}
+
+//------------------------------------
+// Non-member methods
+//------------------------------------
+
+inline void mTransformPlane(const MatrixD& mat, const Point3D& scale, const PlaneD&  plane, PlaneD * result)
+{
+   m_matD_x_scale_x_planeD(mat, &scale.x, &plane.x, &result->x);
+}
+
 #endif //_MMATRIX_H_
